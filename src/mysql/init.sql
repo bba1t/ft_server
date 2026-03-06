01f77711add5ef2abe0c0abeec55b6c22d45ba11
Байтулла Омаров <bba1t@icloud.com>
2026-03-06 17:38:07 +0300

    +monitoring_1

--- src/mysql/init.sql
+++ src/mysql/init.sql
@@ -1,47 +1,39 @@
--- 🗃️ НАСТРОЙКА БАЗ ДАННЫХ И ПОЛЬЗОВАТЕЛЕЙ
+-- НАСТРОЙКА БАЗ ДАННЫХ И ПОЛЬЗОВАТЕЛЕЙ
 
--- Меняем пароль пользователя root на 'rootpassword'
+-- Меняю пароль root на 'rootpassword'
 ALTER USER 'root'@'localhost' IDENTIFIED BY 'rootpassword';
 
-
--- Удаляем всех пользователей с пустым именем
+-- Удаляю всех пользователей с пустым именем
 DELETE FROM mysql.user WHERE User='';
 
-
--- Удаляем стандартную тестовую базу, которая может быть уязвима
--- Удаляем привилегии для тестовой базы
+-- Удаляю стандартную тестовую базу и ее привилегии
 DROP DATABASE IF EXISTS test;
 DELETE FROM mysql.db WHERE Db='test' OR Db='test\\_%';
 
 
--- Создаем базу данных с именем 'wordpress', если она еще не существует
--- CHARACTER SET utf8mb4 поддерживает все символы (включая эмодзи)
--- COLLATE utf8mb4_unicode_ci обеспечивает правильную сортировку
+-- Создаю базу данных WordPress
 CREATE DATABASE IF NOT EXISTS wordpress 
 CHARACTER SET utf8mb4 
 COLLATE utf8mb4_unicode_ci;
 
-
--- Создаем пользователя 'wpuser' для подключения с localhost
--- Создаем того же пользователя для подключения с 127.0.0.1
+-- Создаю пользователя для WordPress, который может подключаться с localhost и по ip
 -- Некоторые приложения используют IP вместо localhost
 CREATE USER IF NOT EXISTS 'wpuser'@'localhost' IDENTIFIED BY 'wppassword';
 CREATE USER IF NOT EXISTS 'wpuser'@'127.0.0.1' IDENTIFIED BY 'wppassword';
 
-
--- Даем пользователю wpuser ВСЕ привилегии на базу wordpress
--- @'localhost' - только для подключений с локальной машины
--- @'127.0.0.1' - только для подключений через IP localhost
+-- Даю пользователю wpuser ВСЕ привилегии на базу wordpress
 GRANT ALL PRIVILEGES ON wordpress.* TO 'wpuser'@'localhost';
 GRANT ALL PRIVILEGES ON wordpress.* TO 'wpuser'@'127.0.0.1';
 
-
--- Создаем отдельную базу для phpMyAdmin (опционально)
+-- Создаю базу для phpMyAdmin
 CREATE DATABASE IF NOT EXISTS phpmyadmin 
 CHARACTER SET utf8mb4 
 COLLATE utf8mb4_unicode_ci;
 
 
--- Применяем все изменения прав пользователей
--- Без этой команды изменения не вступят в силу!
+-- Создаю пользователя для МОНИТРОРИНГА с любого хоста и даю минимальные необходимые права для сбора метрик
+CREATE USER IF NOT EXISTS 'exporter'@'%' IDENTIFIED BY 'exportpassword';
+GRANT PROCESS, REPLICATION CLIENT, SELECT ON *.* TO 'exporter'@'%';
+
+-- Применение всех изменений
 FLUSH PRIVILEGES;
--- start.sh
+++ start.sh
@@ -1,28 +1,31 @@
 #!/bin/bash
 
-# Определяю какой конфиг для nginx server использовать
+# Определяю какой конфиг для nginx server использовать.
 # Поскольку я вставляю настройки в default, нет необходимости создавать для него симлинк, потому что он уже существует.
 if [ "$AUTOINDEX" = "off" ]; then
     cp /tmp/nginx_auto_off.conf /etc/nginx/sites-available/default
+    echo "Автоинтексация отключена"
 else
     cp /tmp/nginx.conf /etc/nginx/sites-available/default
+    echo "Автоинтексация включена"
 fi
 
 
-echo "🚀 Starting MySQL..."
-mysqld_safe --user=mysql &
-sleep 10
+echo "Стартую Mysql"
+service mysql start
+sleep 30
 
-echo "📝 Configuring MySQL..."
+echo "Инициализирую пользователей бд"
 # Скрипт инициализации бд, -u root: подключение как root, < /tmp/init.sql: перенаправление содержимого в mysql
-mysql -u root < /tmp/init.sql
+# Используем сокет для подключения
+mysql -u root -S /var/run/mysqld/mysqld.sock < /tmp/init.sql
+# mysql -u root < /tmp/init.sql
 
 
 # Запускаю в фоне
-echo "🚀 Starting PHP-FPM..."
+echo "Запускаю PHP-FPM"
 php-fpm7.3 --daemonize
 
-
 # Запускаю nginx в FOREGROUND режиме, как главный процесс, чтобы контейнер не упал
-echo "🚀 Starting Nginx..."
+echo "Запускаю Nginx"
 nginx -g 'daemon off;'
